d0b21a31d9fdd2b1247d848ce2f918281131dd45
SEGURADORALIDER\luizmarinho.brq <luizmarinho.brq@seguradoralider.com.br>
2025-01-17 18:35:52 -0300

    Alterando a mensagem da aplicação

--- Aulas/Program.cs
+++ Aulas/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Olá mundo! Alterando o texto da mensagem para ser exibida no GitHubActions!");
